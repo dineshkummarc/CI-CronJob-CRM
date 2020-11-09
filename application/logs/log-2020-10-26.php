7033d4a26112f75fc141530165791c5e29ea6e3f
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-09 09:24:45 +0530

    update

---
+++ application/logs/log-2020-10-26.php
@@ -0,0 +1,65 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-10-26 05:08:44 --> Config Class Initialized
+INFO - 2020-10-26 05:08:44 --> Hooks Class Initialized
+DEBUG - 2020-10-26 05:08:45 --> UTF-8 Support Enabled
+INFO - 2020-10-26 05:08:45 --> Utf8 Class Initialized
+INFO - 2020-10-26 05:08:45 --> URI Class Initialized
+DEBUG - 2020-10-26 05:08:45 --> No URI present. Default controller set.
+INFO - 2020-10-26 05:08:45 --> Router Class Initialized
+INFO - 2020-10-26 05:08:45 --> Output Class Initialized
+INFO - 2020-10-26 05:08:45 --> Security Class Initialized
+DEBUG - 2020-10-26 05:08:45 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-10-26 05:08:45 --> Input Class Initialized
+INFO - 2020-10-26 05:08:45 --> Language Class Initialized
+INFO - 2020-10-26 05:08:45 --> Loader Class Initialized
+DEBUG - 2020-10-26 05:08:45 --> Config file loaded: C:\xampp\htdocs\test1\CI-CronJob-CRM\application\config/pagination.php
+INFO - 2020-10-26 05:08:46 --> Helper loaded: security_helper
+INFO - 2020-10-26 05:08:46 --> Helper loaded: form_helper
+INFO - 2020-10-26 05:08:46 --> Helper loaded: url_helper
+INFO - 2020-10-26 05:08:46 --> Database Driver Class Initialized
+ERROR - 2020-10-26 05:08:46 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'catalin_apiadm'@'localhost' (using password: YES) C:\xampp\htdocs\test1\CI-CronJob-CRM\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-10-26 05:08:46 --> Unable to connect to the database
+INFO - 2020-10-26 05:08:46 --> Language file loaded: language/english/db_lang.php
+INFO - 2020-10-26 05:18:49 --> Config Class Initialized
+INFO - 2020-10-26 05:18:49 --> Hooks Class Initialized
+DEBUG - 2020-10-26 05:18:49 --> UTF-8 Support Enabled
+INFO - 2020-10-26 05:18:49 --> Utf8 Class Initialized
+INFO - 2020-10-26 05:18:49 --> URI Class Initialized
+DEBUG - 2020-10-26 05:18:49 --> No URI present. Default controller set.
+INFO - 2020-10-26 05:18:49 --> Router Class Initialized
+INFO - 2020-10-26 05:18:50 --> Output Class Initialized
+INFO - 2020-10-26 05:18:50 --> Security Class Initialized
+DEBUG - 2020-10-26 05:18:50 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-10-26 05:18:50 --> Input Class Initialized
+INFO - 2020-10-26 05:18:50 --> Language Class Initialized
+INFO - 2020-10-26 05:18:50 --> Loader Class Initialized
+DEBUG - 2020-10-26 05:18:50 --> Config file loaded: C:\xampp\htdocs\test1\CI-CronJob-CRM\application\config/pagination.php
+INFO - 2020-10-26 05:18:50 --> Helper loaded: security_helper
+INFO - 2020-10-26 05:18:50 --> Helper loaded: form_helper
+INFO - 2020-10-26 05:18:50 --> Helper loaded: url_helper
+INFO - 2020-10-26 05:18:50 --> Database Driver Class Initialized
+ERROR - 2020-10-26 05:18:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'catalin_api' C:\xampp\htdocs\test1\CI-CronJob-CRM\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-10-26 05:18:50 --> Unable to connect to the database
+INFO - 2020-10-26 05:18:50 --> Language file loaded: language/english/db_lang.php
+INFO - 2020-10-26 05:19:54 --> Config Class Initialized
+INFO - 2020-10-26 05:19:54 --> Hooks Class Initialized
+DEBUG - 2020-10-26 05:19:54 --> UTF-8 Support Enabled
+INFO - 2020-10-26 05:19:54 --> Utf8 Class Initialized
+INFO - 2020-10-26 05:19:54 --> URI Class Initialized
+DEBUG - 2020-10-26 05:19:54 --> No URI present. Default controller set.
+INFO - 2020-10-26 05:19:54 --> Router Class Initialized
+INFO - 2020-10-26 05:19:54 --> Output Class Initialized
+INFO - 2020-10-26 05:19:54 --> Security Class Initialized
+DEBUG - 2020-10-26 05:19:54 --> Global POST, GET and COOKIE data sanitized
+INFO - 2020-10-26 05:19:54 --> Input Class Initialized
+INFO - 2020-10-26 05:19:54 --> Language Class Initialized
+INFO - 2020-10-26 05:19:55 --> Loader Class Initialized
+DEBUG - 2020-10-26 05:19:55 --> Config file loaded: C:\xampp\htdocs\test1\CI-CronJob-CRM\application\config/pagination.php
+INFO - 2020-10-26 05:19:55 --> Helper loaded: security_helper
+INFO - 2020-10-26 05:19:55 --> Helper loaded: form_helper
+INFO - 2020-10-26 05:19:55 --> Helper loaded: url_helper
+INFO - 2020-10-26 05:19:55 --> Database Driver Class Initialized
+ERROR - 2020-10-26 05:19:55 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'catalin_api' C:\xampp\htdocs\test1\CI-CronJob-CRM\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2020-10-26 05:19:55 --> Unable to connect to the database
+INFO - 2020-10-26 05:19:55 --> Language file loaded: language/english/db_lang.php
